f0b147cebb826e64c64170456b91b4256622818e
mh-dpi <honey.app.developer@gmail.com>
2025-01-04 08:24:22 +0600

    Variables

--- bin/practice.dart
+++ bin/practice.dart
@@ -1,3 +1,21 @@
 void main(){
   print("Hello World");
+ var name = "Mahmudul Hasan";
+ var year = 2024;
+ var antennaDiameter = 3.6;
+ var flyObjects = ['Jupiter','Saturn','Uranus','Naptune'];
+ var image = {
+   'tags':['saturn'],
+   'url':'//path/to/saturn.jpg'
+ };
+
+ print(name);
+ print(year);
+ print(antennaDiameter);
+ print(flyObjects);
+ print(image);
+
+
+
+
 }
